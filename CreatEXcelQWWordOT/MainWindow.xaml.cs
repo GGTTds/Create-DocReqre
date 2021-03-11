659079d10477c348da0b9e549c093ad1f9277147
tpnotllnptl@gmail.com <tpnotllnptl@gmail.com>
2021-03-11 13:13:49 +0500

    #WD-2 Конструкция окна для отчета

--- CreatEXcelQWWordOT/MainWindow.xaml.cs
+++ CreatEXcelQWWordOT/MainWindow.xaml.cs
@@ -24,5 +24,26 @@ namespace CreatEXcelQWWordOT
         {
             InitializeComponent();
         }
+
+        private void CreaOtch_Click(object sender, RoutedEventArgs e)
+        {
+            CreateOtcEx WW = new CreateOtcEx();
+            WW.Show();
+            this.Close();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            EditProd WW = new EditProd();
+            WW.Show();
+            this.Close();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            AddPro WW = new AddPro();
+            WW.Show();
+            this.Close();
+        }
     }
 }
